2c27f220e6a96992d3eec22f0112fc2279d0dc70
Jason Chen <purplvampire@gmail.com>
2024-10-04 20:34:09 +0800

    Initial Commit

---
+++ AnimatedSlidingPopover/AnimatedSlidingPopoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedSlidingPopoverApp.swift
+//  AnimatedSlidingPopover
+//
+//  Created by 陳信彰 on 2024/10/4.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedSlidingPopoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedSlidingPopover/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimatedSlidingPopover
+//
+//  Created by 陳信彰 on 2024/10/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
